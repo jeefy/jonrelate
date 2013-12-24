0130cf755e58da3b0d0864b4417dc933a977115f
Matt Mowers <matt.mowers@gmail.com>
2013-12-23 23:36:56 -0800

    features added, cleaner homologene autofill

--- sites/all/themes/subboot/template.php
+++ sites/all/themes/subboot/template.php
@@ -1,14 +1,11 @@
 <?php
 
-/*function subboot_preprocess_html(&$variables,$hook) {
+function subboot_preprocess_html(&$variables,$hook) {
   $path = drupal_get_destination();
   $path_cur = current_path();
   $request_path = request_path();
 
-  if(preg_match('#^node/add/local-biz$#', $path_cur)){
-    global $user;
-    if(!in_array('administrator',$user->roles)){
-      drupal_add_js(path_to_theme() . '/js/advertiser.js',array('type' => 'file', 'scope' => 'footer', 'weight' => 100, 'group' => JS_THEME));      
-    }
-  }
-}*/
+  //if(preg_match('#^node/add/causal-relationship$#', $path_cur)){
+  //  drupal_add_js(path_to_theme() . '/js/add-relationship.js',array('type' => 'file', 'scope' => 'footer', 'weight' => 100, 'group' => JS_THEME));      
+  //}
+}
