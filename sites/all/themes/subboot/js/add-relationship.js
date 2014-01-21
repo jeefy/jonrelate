35e5e9a2b1a36d5305d92766b8c1ef8d4a3a4c7e
Matt Mowers <matt.mowers@gmail.com>
2014-01-20 23:46:00 -0800

    many changes...

--- sites/all/themes/subboot/js/add-relationship.js
+++ sites/all/themes/subboot/js/add-relationship.js
@@ -1,62 +1,134 @@
 jQuery(document).ready(function($) {
-  $('.page-node-add-causal-relationship #edit-field-cause-und').change(function(){
-    var baseurl = window.location.protocol + '//' + window.location.hostname;
-    $.get(baseurl+'/search-homologene', 'search-homologene-string='+$(this).val(), function(data){
-      if(data != "null" && data != ""){
-        data_json = $.parseJSON(data);
-        $('#edit-field-cause-homologene-und').hide();
-        $('#edit-field-cause-homologene-und').before('<select id="cause-homologene-select" multiple="multiple"></select>');
-        for(i=0; i<data_json.length; i++){
-          $('#cause-homologene-select').append('<option>'+data_json[i].id+': '+data_json[i].name+'</option>');
-        }
-        $('#cause-homologene-select').append('<option>None of these</option>');
-        $('#cause-homologene-select').focus();      
+  $('#edit-field-cause-und, #edit-field-effect-und').change(function(){
+    if($(this).val().length > 2){
+      if($(this).attr('id') == 'edit-field-cause-und'){
+        var causeOrEffect = 'cause';
       }
-      //$('.page-node-add-causal-relationship #edit-field-cause-und').after(data);
-    });
-    /*$.get('https://www.google.com/', null, function(text){
-      alert($(text).find('#gbqfsa'));
-    });*/
-    /*$.get('http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=homologene&term='+$(this).val()+'&retmode=xml', null, function(text){
-      //var xml = $.parseXML(text);
-      var id_array = $(text).find('id');
-      var id_1 = $(text).find('id')[0];
-      var b =4;
-      //alert($(text).find('id'));
-      alert(text);
-    });*/
-    /*$('.eutils-search').remove();
-    $(this).after('<div class="eutils-search"><iframe src="http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=homologene&term='+$(this).val()+'"></iframe></div>');
-    */
-  });
-  $('body').on('change','#cause-homologene-select', function(){
-    if($(this).val() != 'None of these'){
-      $('#edit-field-cause-homologene-und').val($(this).val());
+      else{
+        var causeOrEffect = 'effect';
+      }
+      var baseurl = window.location.protocol + '//' + window.location.hostname;
+      $('.'+causeOrEffect+'-homologene-searching').remove();
+      
+      //search first for the specific thing
+      var db_selected = $('#edit-field-'+causeOrEffect+'-category-und option:selected').text().toLowerCase();
+      var db_search = 'homologene';
+      if(db_selected == 'homologene' ||
+         db_selected == 'gene' ||
+         db_selected == 'protein' ||
+         db_selected == 'small molecule' ||
+         db_selected == 'transcript'){
+        switch (db_selected){
+          case 'homologene': db_search="homologene";break;
+          case 'gene': db_search="gene";break;
+          case 'protein': db_search="protein";break;
+          case 'small molecule': db_search="pccompound";break;
+          case 'transcript': db_search="nuccore";break;
+        }
+        $('#edit-field-'+causeOrEffect+'-und').after('<p class="'+causeOrEffect+'-searching">Searching NCBI '+db_search+' database...</p>');
+        $.get(baseurl+'/search-ncbi', 'search-ncbi-string='+$(this).val()+'&database='+db_search, function(data){
+          $('.'+causeOrEffect+'-searching').remove();
+          $('#'+causeOrEffect+'-select').remove();
+          if(data != "null" && data != ""){
+            data_json = $.parseJSON(data);
+            $('#edit-field-'+causeOrEffect+'-und').after('<br><select id="'+causeOrEffect+'-select" multiple="multiple"></select>');
+            for(i=0; i<data_json.length; i++){
+              $('#'+causeOrEffect+'-select').append('<option value="'+data_json[i].id+'">'+data_json[i].id+data_json[i].name+'</option>');
+            }
+            $('#'+causeOrEffect+'-select').append('<option>None of these</option>');            
+            $('#'+causeOrEffect+'-select').focus();      
+          }
+          else{
+            $('#edit-field-'+causeOrEffect+'-und').after('<p class="'+causeOrEffect+'-searching"><em>Sorry, no results found in NCBI HomoloGene database</em></p>');            
+          }
+        });
+
+      }
+      
+      //next, search for the homologene
+      $('#edit-field-'+causeOrEffect+'-homologene-und').after('<p class="'+causeOrEffect+'-homologene-searching">Searching NCBI homologene database...</p>');
+      $.get(baseurl+'/search-ncbi', 'search-ncbi-string='+$(this).val()+'&database=homologene', function(data){
+        $('.'+causeOrEffect+'-homologene-searching').remove();
+        $('#'+causeOrEffect+'-homologene-select').remove();
+        if(data != "null" && data != ""){
+          data_json = $.parseJSON(data);
+          //$('#edit-field-cause-homologene-und').hide();
+          $('#edit-field-'+causeOrEffect+'-homologene-und').after('<br><select id="'+causeOrEffect+'-homologene-select" multiple="multiple"></select>');
+          for(i=0; i<data_json.length; i++){
+            $('#'+causeOrEffect+'-homologene-select').append('<option value="'+data_json[i].id+'">'+data_json[i].id+data_json[i].name+'</option>');
+          }
+          $('#'+causeOrEffect+'-homologene-select').append('<option>None of these</option>');            
+          $('#'+causeOrEffect+'-homologene-select').focus();      
+        }
+        else{
+          $('#edit-field-'+causeOrEffect+'-homologene-und').after('<p class="'+causeOrEffect+'-homologene-searching"><em>Sorry, no results found in NCBI HomoloGene database</em></p>');            
+        }
+      });
+      /*$.get('https://www.google.com/', null, function(text){
+        alert($(text).find('#gbqfsa'));
+      });*/
+      /*$.get('http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=homologene&term='+$(this).val()+'&retmode=xml', null, function(text){
+        //var xml = $.parseXML(text);
+        var id_array = $(text).find('id');
+        var id_1 = $(text).find('id')[0];
+        var b =4;
+        //alert($(text).find('id'));
+        alert(text);
+      });*/
+      /*$('.eutils-search').remove();
+      $(this).after('<div class="eutils-search"><iframe src="http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=homologene&term='+$(this).val()+'"></iframe></div>');
+      */
     }
-    $('#edit-field-cause-homologene-und').show();
-    $('#edit-field-cause-homologene-und').focus();
+  });
+  $('body').on('change','#cause-select, #effect-select, #cause-homologene-select, #effect-homologene-select', function(){
+    if($(this).attr('id') == 'cause-homologene-select' || $(this).attr('id') == 'cause-select'){
+      var causeOrEffect = 'cause';
+    }
+    else{
+      var causeOrEffect = 'effect';
+    }
+    if($(this).attr('id') == 'cause-homologene-select' || $(this).attr('id') == 'effect-homologene-select'){
+      var homolOrNot = '-homologene';
+    }
+    else{
+      var homolOrNot = '';
+    }
+    if($(this).val() != 'None of these'){
+      $('#edit-field-'+causeOrEffect+homolOrNot+'-und').val($(this).val());
+    }
+    $('#edit-field-'+causeOrEffect+homolOrNot+'-und').show();
+    $('#edit-field-'+causeOrEffect+homolOrNot+'-und').focus();
     $(this).remove();
   });
-  $('body').on('blur','#cause-homologene-select', function(){
-    $('#edit-field-cause-homologene-und').show();
+  /*$('body').on('blur','#cause-homologene-select, #effect-homologene-select', function(){
+    if($(this).attr('id') == 'cause-homologene-select'){
+      var causeOrEffect = 'cause';
+    }
+    else{
+      var causeOrEffect = 'effect';
+    }
+    $('#edit-field-'+causeOrEffect+'-homologene-und').show();
     $(this).remove();
-  });
+  });*/
 
   //do the same for effect
-  $('.page-node-add-causal-relationship #edit-field-effect-und').change(function(){
-    var baseurl = window.location.protocol + '//' + window.location.hostname;
-    $.get(baseurl+'/search-homologene', 'search-homologene-string='+$(this).val(), function(data){
-      if(data != "null" && data != ""){
-        data_json = $.parseJSON(data);
-        $('#edit-field-effect-homologene-und').hide();
-        $('#edit-field-effect-homologene-und').before('<select id="effect-homologene-select" multiple="multiple"></select>');
-        for(i=0; i<data_json.length; i++){
-          $('#effect-homologene-select').append('<option>'+data_json[i].id+': '+data_json[i].name+'</option>');
+  /*$('.page-node-add-causal-relationship #edit-field-effect-und').change(function(){
+    if($(this).val().length > 2){
+      var baseurl = window.location.protocol + '//' + window.location.hostname;
+      $.get(baseurl+'/search-homologene', 'search-homologene-string='+$(this).val(), function(data){
+        if(data != "null" && data != ""){
+          data_json = $.parseJSON(data);
+          $('#effect-homologene-select').remove();
+          //$('#edit-field-effect-homologene-und').hide();
+          $('#edit-field-effect-homologene-und').after('<br><select id="effect-homologene-select" multiple="multiple"></select>');
+          for(i=0; i<data_json.length; i++){
+            $('#effect-homologene-select').append('<option value="'+data_json[i].id+'">'+data_json[i].id+': '+data_json[i].name+'</option>');
+          }
+          $('#effect-homologene-select').append('<option>None of these</option>');
+          $('#effect-homologene-select').focus();      
         }
-        $('#effect-homologene-select').append('<option>None of these</option>');
-        $('#effect-homologene-select').focus();      
-      }
-    });
+      });      
+    }
   });
   $('body').on('change','#effect-homologene-select', function(){
     if($(this).val() != 'None of these'){
@@ -69,5 +141,5 @@ jQuery(document).ready(function($) {
   $('body').on('blur','#effect-homologene-select', function(){
     $('#edit-field-effect-homologene-und').show();
     $(this).remove();
-  });
+  });*/
 });
--- sites/all/themes/subboot/template.php
+++ sites/all/themes/subboot/template.php
@@ -5,8 +5,10 @@ function subboot_preprocess_html(&$variables,$hook) {
   $path_cur = current_path();
   $request_path = request_path();
 
-  if(preg_match('#^node/add/causal-relationship$#', $path_cur)){
+  if(preg_match('#^node/add/causal-relationship$#', $path_cur) ||
+     preg_match('#^node/[1-9][0-9]*/edit$#', $path_cur)){
     drupal_add_js(path_to_theme() . '/js/add-relationship.js',array('type' => 'file', 'scope' => 'footer', 'weight' => 100, 'group' => JS_THEME));      
+    drupal_add_css(path_to_theme() . '/css/add-relationship.css', array('weight' => CSS_THEME));
   }
   elseif(preg_match('#^connections$#', $path_cur)){
     drupal_add_js(path_to_theme() . '/js/connections.js',array('type' => 'file', 'scope' => 'footer', 'weight' => 100, 'group' => JS_THEME));      
