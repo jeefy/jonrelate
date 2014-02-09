5bc2ba5b367650e41f5257b540b9010fc5e90b99
Matt Mowers <matt.mowers@gmail.com>
2014-02-08 19:42:59 -0800

    adding cytoscape nodes and edges for each relationship shown

--- sites/all/themes/subboot/js/connections.js
+++ sites/all/themes/subboot/js/connections.js
@@ -1,51 +1,6 @@
 jQuery(document).ready(function($){
-  function getUrlVars(){
-    var vars = [], hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for(var i = 0; i < hashes.length; i++){
-      hash = hashes[i].split('=');
-      vars.push(hash[0]);
-      vars[hash[0]] = decodeURIComponent(hash[1]);
-    }
-    return vars;
-  }
-  $('#edit-field-effect-tid-wrapper').hide();
-  $('.view-connections .view-content').before('<div class="connections-from-addition"></div>');
-  var field_effect_tid = getUrlVars()["field_effect_tid"];
-  if(field_effect_tid != null){
-    $('.views-row').each(function(index){
-      var action = $(this).find(".field-action").text();
-      var effect = $(this).find(".field-effect-class").text();
-      if(effect.toLowerCase() == field_effect_tid.toLowerCase()){
-        if(action == 'Decreases'){
-          var decreased_action = $('.connections-from-addition .action-decreased');
-          if(!(decreased_action.length)){
-            $('.connections-from-addition').append('<h3 class="action-decreased">Is Decreased by</h3>');
-          }
-          $('.connections-from-addition').append($(this));
-        }        
-        if(action == 'Increases'){
-          var decreased_action = $('.connections-from-addition .action-increased');
-          if(!(decreased_action.length)){
-            $('.connections-from-addition').append('<h3 class="action-increased">Is Increased by</h3>');
-          }
-          $('.connections-from-addition').append($(this));
-        }        
-        if(action == "Doesn't change"){
-          var decreased_action = $('.connections-from-addition .action-none');
-          if(!(decreased_action.length)){
-            $('.connections-from-addition').append('<h3 class="action-none">Is not changed by</h3>');
-          }
-          $('.connections-from-addition').append($(this));
-        }        
-      }
-    });
-  }
-  //$('.view-connections .view-content').css('float','right');
-  $('#views-exposed-form-connections-page').submit(function(){
-    $('#edit-field-effect-tid').val($('#edit-field-cause-tid').val());
-  });
 
+  //add cytoscape web functionality
   $('.view-connections').after('<br><br><div id="working-model-head">Working model<hr></div><div id="cytoscapeweb"></div><div id="note"><p>Click nodes or edges.</p></div>');
   var div_id = "cytoscapeweb";
   
@@ -53,22 +8,54 @@ jQuery(document).ready(function($){
   var network_json = {
           // you need to specify a data schema for custom attributes!
           dataSchema: {
-              nodes: [ { name: "label", type: "string" },
-                       { name: "foo", type: "string" }
+              nodes: [ { name: "label", type: "string" }
                   ],
-              edges: [ { name: "label", type: "string" },
-                       { name: "bar", type: "string" }
+              edges: [ { name: "label", type: "string" }
               ]
           },
           // NOTE the custom attributes on nodes and edges
           data: {
-              nodes: [ { id: "1", label: "1", foo: "Is this the real life?" },
-                       { id: "2", label: "2", foo: "Is this just fantasy?" }
+              nodes: [ { id: "1", label: "1"},
+                       { id: "2", label: "2"}
               ],
-              edges: [ { id: "2to1", target: "1", source: "2", label: "2 to 1", bar: "Caught in a landslide..." }
+              edges: [ { id: "2to1", target: "1", source: "2", label: "2 to 1"}
               ]
           }
   };
+  //add nodes and edges for each connection
+  $('.views-row').each(function(index, value){
+    //add nodes for each cause term, if they don't already exist.
+    var cause_term = $(this).find('.field-cause-class').text();
+    var cause_exists = false;
+    for(var i=0; i<network_json.data.nodes.length; i++){
+      if(cause_term == network_json.data.nodes[i].label){
+        cause_exists = true;
+        break;
+      }
+    }
+    if(!cause_exists){
+      network_json.data.nodes.push({id: cause_term, label: cause_term});
+    }
+
+    //add nodes for each effect term, if they don't already exist.
+    var effect_term = $(this).find('.field-effect-class').text();
+    var effect_exists = false;
+    for(var i=0; i<network_json.data.nodes.length; i++){
+      if(effect_term == network_json.data.nodes[i].label){
+        effect_exists = true;
+        break;
+      }
+    }
+    if(!effect_exists){
+      network_json.data.nodes.push({id: effect_term, label: effect_term});
+    }
+
+    //add edge no matter what
+    var relationship = $(this).find('.field-action').text();
+    var edge_id = cause_term+'_'+relationship+'_'+effect_term+'_'+index;
+    var edge_label = cause_term+' '+relationship+' '+effect_term;
+    network_json.data.edges.push({id: edge_id, target: effect_term, source: cause_term, label: edge_label});
+  });
   
   // initialization options
   var options = {
@@ -119,4 +106,61 @@ jQuery(document).ready(function($){
   };
   
   vis.draw(draw_options);
+
+
+
+
+  //this function is for getting the variables sent in the url. Used below to get the search term.
+  function getUrlVars(){
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++){
+      hash = hashes[i].split('=');
+      vars.push(hash[0]);
+      vars[hash[0]] = decodeURIComponent(hash[1]);
+    }
+    return vars;
+  }
+
+  //group the results based on if they cause or are caused by the search term
+  $('.view-connections .view-content').before('<div class="connections-from-addition"></div>');
+  var field_effect_tid = getUrlVars()["field_effect_tid"];
+  if(field_effect_tid != null){
+    $('.views-row').each(function(index){
+      var action = $(this).find(".field-action").text();
+      var effect = $(this).find(".field-effect-class").text();
+      if(effect.toLowerCase() == field_effect_tid.toLowerCase()){
+        if(action == 'Decreases'){
+          var decreased_action = $('.connections-from-addition .action-decreased');
+          if(!(decreased_action.length)){
+            $('.connections-from-addition').append('<h3 class="action-decreased">Is Decreased by</h3>');
+          }
+          $('.connections-from-addition').append($(this));
+        }        
+        if(action == 'Increases'){
+          var decreased_action = $('.connections-from-addition .action-increased');
+          if(!(decreased_action.length)){
+            $('.connections-from-addition').append('<h3 class="action-increased">Is Increased by</h3>');
+          }
+          $('.connections-from-addition').append($(this));
+        }        
+        if(action == "Doesn't change"){
+          var decreased_action = $('.connections-from-addition .action-none');
+          if(!(decreased_action.length)){
+            $('.connections-from-addition').append('<h3 class="action-none">Is not changed by</h3>');
+          }
+          $('.connections-from-addition').append($(this));
+        }        
+      }
+    });
+  }
+
+  //hide effect search box and autofill with cause box when submitting search form
+  $('#edit-field-effect-tid-wrapper').hide();
+  //$('.view-connections .view-content').css('float','right');
+  $('#views-exposed-form-connections-page').submit(function(){
+    $('#edit-field-effect-tid').val($('#edit-field-cause-tid').val());
+  });
+
+
 });
